55642c9fe860c319deb075677bd57125d683032f
Christy Carwile <carwilcl@gmail.com>
2021-05-02 19:14:23 -0400

    Add LocalStorage helper

--- tabletop-tools/src/core/utils/cookies.ts
+++ tabletop-tools/src/core/utils/cookies.ts
@@ -1,5 +1,30 @@
 import Cookies from 'js-cookie';
 
+export class LocalStorage {
 
-Cookies.set('myCat', 'Pacman', { path: '/' });
-console.log(Cookies.get('myCat')); // Pacman
+  public static set<T = any> (key: string, data: T, expirationInDays?: number): void {
+    let stringifiedData: string | undefined = undefined;
+    if (data instanceof Object) {
+      stringifiedData = JSON.stringify(data);
+    }
+    if (expirationInDays) {
+      Cookies.set(key, stringifiedData || data, {expires: expirationInDays});
+      return;
+    }
+    Cookies.set(key, stringifiedData || data);
+  }
+
+  public static read<T = any> (key: string): T {
+    const value = Cookies.get(key);
+    try {
+      const destringifiedData = JSON.parse(value);
+      if (destringifiedData instanceof Object) {
+        return destringifiedData as T;
+      }
+      return value as T;
+    } catch (e) {
+      // It's not JSON
+      return value as T;
+    }
+  }
+}
