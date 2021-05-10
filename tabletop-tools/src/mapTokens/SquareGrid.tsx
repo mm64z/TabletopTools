95698700d1a510b82d7fd81daf0eab0fdb54a42b
Michael Matthews <michael.64.matthews@gmail.com>
2021-05-10 19:10:40 -0400

    Display drag distance

--- tabletop-tools/src/mapTokens/SquareGrid.tsx
+++ tabletop-tools/src/mapTokens/SquareGrid.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { Rnd, DraggableData, Position, RndDragEvent } from 'react-rnd';
 import Silverflame from '../resources/Silverflame-quick.png';
 
+import './SquareGrid.scss';
+
 
 class SquareGrid extends React.Component {
 
@@ -24,12 +26,20 @@ class SquareGrid extends React.Component {
     });
   }
 
+  componentDidMount() {
+        document.addEventListener("contextmenu", this.handleContextMenu);
+    }
+
+    componentWillUnmount() {
+        document.removeEventListener("contextmenu", this.handleContextMenu);
+    }
+
 
   startMovement = ( e: RndDragEvent, data: DraggableData): void => {
     console.log("start ", data.x, data.y);
      this.setState({
-      dragArrowStart: {x: data.x-23, y: data.y+8},
-      dragArrowCurrent: {x: data.x-23, y: data.y+8},
+      dragArrowStart: {x: data.x+32, y: data.y+32},
+      dragArrowCurrent: {x: data.x+32, y: data.y+32},
       dragArrowDragging: true
     });
   }
@@ -47,10 +57,20 @@ class SquareGrid extends React.Component {
     });
   }
 
+  calcDistance (xSquares: number, ySquares: number) {
+    const diagonalSquares = Math.min(xSquares, ySquares);
+    const straightSquares = Math.max(xSquares, ySquares) - diagonalSquares;
+    // Distance = MIN+ROUNDDOWN(MIN/2) + MAX-MIN
+    // distance is free diagonals
+    return diagonalSquares + Math.trunc(diagonalSquares/2) + straightSquares;
+  }
+
 
   render() {
-    console.log("render ", this.state);
     const gridSize = 64;
+    const ySquares = Math.abs(this.state.dragArrowCurrent.y-this.state.dragArrowStart.y)/gridSize;
+    const xSquares = Math.abs(this.state.dragArrowCurrent.x-this.state.dragArrowStart.x)/gridSize;
+    const distance = this.calcDistance(xSquares, ySquares);
     return (
       <div className="SquareGrid" style={{position: 'relative', width:'100%',height:'800px'}}>
 
@@ -82,15 +102,22 @@ class SquareGrid extends React.Component {
               <path d={`M ${gridSize*5} 0 L 0 0 0 ${gridSize*5}`} fill="none" stroke="gray" strokeWidth="1"/>
             </pattern>
             <marker id="arrowhead" markerWidth="10" markerHeight="7"
-              refX="0" refY="3.5" orient="auto">
+              refX="10" refY="3.5" orient="auto">
               <polygon points="0 0, 10 3.5, 0 7" />
             </marker>
           </defs>
           {this.state.dragArrowDragging ?
-          <line x1={this.state.dragArrowStart.x} y1={this.state.dragArrowStart.y}
+          <g>
+            <line x1={this.state.dragArrowStart.x} y1={this.state.dragArrowStart.y}
                 x2={this.state.dragArrowCurrent.x} y2={this.state.dragArrowCurrent.y}
                 style={{stroke:'rgb(0,0,0)', strokeWidth:'2'}}
-                markerEnd="url(#arrowhead)"/> : false }
+                markerEnd="url(#arrowhead)"/>
+            <text transform={"translate(" + (this.state.dragArrowStart.x + this.state.dragArrowCurrent.x)/2 + ", " +
+                  (this.state.dragArrowStart.y + this.state.dragArrowCurrent.y)/2 + ") " +
+                  "rotate(" + Math.atan((this.state.dragArrowCurrent.y-this.state.dragArrowStart.y)/(this.state.dragArrowCurrent.x-this.state.dragArrowStart.x))*180/Math.PI + ")"}
+                  >{distance} sq</text>
+          </g>
+                 : false }
 
           <rect width="100%" height="100%" fill="url(#grid)" />
         </svg>
